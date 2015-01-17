d2604e7f007a77c319714e5023c48d142fff5f53
WFT <will.a.ft@gmail.com>
2015-01-17 04:22:25 -0500

    location fun

---
+++ Player.swift
@@ -0,0 +1,26 @@
+//
+//  Player.swift
+//  Fence
+//
+//  Created by Will on 1/17/15.
+//  Copyright (c) 2015 Arani Bhattacharyay. All rights reserved.
+//
+
+import CoreLocation
+import UIKit
+
+class Player {
+    var image: UIImage?
+    var target: Int?
+    var hunter: Int?
+    
+    /*
+    var requiredAccuracy: CLLocationAccuracy {
+        // if we're playing, bump the accuracy
+        return (target != nil || hunter != nil) ? kCLLocationAccuracyBest : kCLLocationAccuracyHundredMeters
+    }*/
+    
+    init() {
+        
+    }
+}
